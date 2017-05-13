dafb0ac6bfaebaa48f5339f00e0a46065f516737
TaoMonkey <1241705275@qq.com>
2017-05-13 11:05:02 +0800

    加入数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,48 @@
+package com.coolweather.android.db;
+
+/**
+ * Created by taoguo on 2017/5/13.
+ */
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport{
+
+    private int id;
+    private String cityName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    private String cityCode;
+    private int provinceId;
+}
